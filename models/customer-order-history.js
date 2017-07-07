1587a096cbfc470e1d4a70c7304c6dbe99ca1c02
Kyle Whitaker <kylewhitaker51@gmail.com>
2017-07-07 13:47:36 -0400

    add product & order model mappings

--- models/customer-order-history.js
+++ models/customer-order-history.js
@@ -1,9 +1,12 @@
+var Product = require('./product');
+var Order = require('./order');
+
 class CustomerOrderHistory {
 
   constructor(input) {
     this.input = input;
-    this.products = null;
-    this.orders = null;
+    this.products = [];
+    this.orders = [];
     this.summary = null;
   }
 
@@ -19,7 +22,28 @@ class CustomerOrderHistory {
   }
 
   createSummary() {
+    this.input.split('\n').forEach(line => {
+      if (line.startsWith('PRODUCT')) {
+        this.addProduct(line);
+      }
+      else if (line.startsWith('ORDER')) {
+        this.addOrder(line);
+      }
+    });
+  }
 
+  addProduct(line) {
+    var details = line.split(' ');
+    var name = details[1];
+    var unitPrice = details[2];
+    this.products.push(new Product(name, unitPrice));
+  }
+
+  addOrder(line) {
+    var details = line.split(' ') as Array;
+    var date = details[1];
+    var lineItems = details.slice(2);
+    this.products.push(new Order(date, lineItems));
   }
 
 }
